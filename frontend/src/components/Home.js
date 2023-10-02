14ce04b0837ccc55a9c60047d9391cfb0d71e2b1
Albert <s95551@pollub.edu.pl>
2023-10-02 20:40:23 +0200

    usuniecie video, gradient

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,10 +7,7 @@ import Cards from './Cards';
 const Home = () => {
     return (
         <>
-      <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
       <Cards />
-      </div>
     </>
   );
   };
