d1308ea9dd9b644f20644f60896295d3a7669cb8
Albert <s95551@pollub.edu.pl>
2023-11-24 19:23:50 +0100

    poprawa asynchronicznosci logowania i rejestracji

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,75 +3,88 @@ const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
 const createUser = async (req, res) => {
-    const name = req.body.name;
-    const surname = req.body.surname;
-    const email = req.body.email;
-    const password = req.body.password;
-    const phoneNumber = req.body.phoneNumber;
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const name = req.body.name;
+  const surname = req.body.surname;
+  const email = req.body.email;
+  const password = req.body.password;
+  const phoneNumber = req.body.phoneNumber;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const nameRegex = /^[A-Z][a-zA-Z]+$/;
-    const surnameRegex = /^[A-Z][a-zA-Z-]*$/;
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,}$/;
-    const phoneNumberRegex = /^\d{9}$/;
+  const nameRegex = /^[A-Z][a-zA-Z]+$/;
+  const surnameRegex = /^[A-Z][a-zA-Z-]*$/;
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,}$/;
+  const phoneNumberRegex = /^\d{9}$/;
 
-    if (!nameRegex.test(name)) {
-      return res.status(400).json({ message: "Invalid name format." });
-    }
-    if (!surnameRegex.test(surname)) {
-      return res.status(400).json({ message: "Invalid surname format." });
-    }
-    if (!emailRegex.test(email)) {
-      return res.status(400).json({ message: "Invalid email format." });
-    }
-    if (!passwordRegex.test(password)) {
-      return res.status(400).json({ message: "Invalid password format." });
-    }
-    if (!phoneNumberRegex.test(phoneNumber)) {
-      return res.status(400).json({ message: "Invalid phone number format." });
-    }
+  if (!nameRegex.test(name)) {
+    return res.status(400).json({ message: "Invalid name format." });
+  }
+  if (!surnameRegex.test(surname)) {
+    return res.status(400).json({ message: "Invalid surname format." });
+  }
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: "Invalid email format." });
+  }
+  if (!passwordRegex.test(password)) {
+    return res.status(400).json({ message: "Invalid password format." });
+  }
+  if (!phoneNumberRegex.test(phoneNumber)) {
+    return res.status(400).json({ message: "Invalid phone number format." });
+  }
+  const user = await User.findOne({ email: email });
 
-    const user = await User.findOne({"email": email});
+  if (!user) {
+    const newUser = new User({
+      name: name,
+      surname: surname,
+      email: email,
+      password: hashedPassword,
+      phoneNumber: phoneNumber,
+    });
 
-    if(!user){
-      const newUser = new User({
-        name: name,
-        surname: surname,
-        email: email,
-        password: hashedPassword,
-        phoneNumber: phoneNumber
-      });
-
-      newUser.save()
-      .then(() => {
-        return res.status(200).json({ message: "User registered."});
-      })
+    try {
+      await newUser.save();
+      return res.status(200).json({ message: "User registered." });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Error registering user.", error: error.message });
     }
-    return res.status(400).json({ message: "Email already taken."});
-}
+  } else {
+    return res.status(400).json({ message: "Email already taken." });
+  }
+};
 
 const loginUser = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+
   try {
-    const user = await User.findOne({"email": email});
+    const user = await User.findOne({ "email": email });
     if (!user) {
-      return res.status(404).json({ message: 'There no user associated with this email address.' });
+      return res
+        .status(404)
+        .json({ message: "User not found with the provided email." });
     }
-    const passwordMatch = bcrypt.compare(password, user.password);
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
     if (passwordMatch) {
-      const token = jwt.sign({ _id: user._id }, password, {expiresIn: "7d",})
-      console.log(token)
-      return res.status(200).json({token, message: "Succesfully logged in."});
+      const token = jwt.sign({ _id: user._id }, password, { expiresIn: "7d" });
+      console.log(token);
+      return res
+        .status(200)
+        .json({ token, message: "Successfully logged in." });
     } else {
-      return res.status(401).json({ error: 'Given password is incorrect' });
+      return res.status(401).json({ error: 'Incorrect password.' });
     }
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).json({ message: 'There was an error during login' });
+    res
+      .status(500)
+      .json({ message: "There was an error during the login process." });
   }
-}
+};
+
 
 const findUserByEmail = async(req,res) =>{
     try {
