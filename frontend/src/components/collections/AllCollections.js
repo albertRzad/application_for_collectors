0a7f5073d4aa5c1a760aecd3a3d62bdd2a333d03
Albert <s95551@pollub.edu.pl>
2023-11-25 21:11:30 +0100

    wyswitlanie obrazkow dla wszystkich kolekcji w ExploreCollections

--- frontend/src/components/collections/AllCollections.js
+++ frontend/src/components/collections/AllCollections.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AllCollections extends Component {
-    render(){
-      const { collections } = this.props;
+  render() {
+    const { collections } = this.props;
 
-      return (
-        <div className='collectionContainers'>
-            {collections.map((collection, index) => (
-              <div key={index} className="collection">
-                <p>Name: {collection.name}</p>
-                <p>Type: {collection.type}</p>
-                <p>Description: {collection.description}</p>
-              </div>
-            ))}
+    return (
+      <div className="collectionContainers">
+        {collections.map((collection, index) => (
+          <div key={index} className="collection">
+            <p>Name: {collection.name}</p>
+            <p>Type: {collection.type}</p>
+            <p>Description: {collection.description}</p>
+            {collection.image === "" || collection.image === null ? (
+              ""
+            ) : (
+              <img width={100} height={100} src={collection.image} />
+            )}
           </div>
-      );
-    }
+        ))}
+      </div>
+    );
+  }
 }
 
 export default AllCollections;
