76772ad2a9809531b2af5a29bf12c61e3c72591d
Albert <s95551@pollub.edu.pl>
2023-11-27 11:51:26 +0100

    naprawa lajkow: teraz przycisk znika po klinieciu i stan zachowuje sie po wylogowaniu albo odswiezeniu strony

--- frontend/src/components/collections/AllCollections.js
+++ frontend/src/components/collections/AllCollections.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class AllCollections extends Component {
+  constructor(props) {
+    super(props);
+
+    const likedCollections = new Set(JSON.parse(localStorage.getItem('likedCollections') || '[]'));
+
+    this.state = {
+      likedCollections,
+    };
+  }
+
   handleCollectionClick = (collectionId) => {
     window.location.href = `http://localhost:3001/exploreCollection/${collectionId}`;
   };
@@ -22,51 +32,48 @@ class AllCollections extends Component {
 
     axios(config)
       .then((response) => {
-        console.log(response);
         if (response.status === 200) {
-          console.log(response.status);
+          this.setState((prevState) => {
+            const updatedLikedCollections = new Set(prevState.likedCollections.add(collectionId));
+            
+            localStorage.setItem('likedCollections', JSON.stringify(Array.from(updatedLikedCollections)));
+
+            return { likedCollections: updatedLikedCollections };
+          });
         }
       })
       .catch((error) => {
         console.error(error);
       });
-  }
+  };
 
   render() {
     const { collections } = this.props;
+    const { likedCollections } = this.state;
 
     return (
       <div className="collectionContainers">
         {collections.map((collection, index) => (
-          <div
-            key={index}
-            className="collection"
-          >
+          <div key={index} className="collection">
             <p>Name: {collection.name}</p>
             <p>Type: {collection.type}</p>
             <p>Description: {collection.description}</p>
             <p>Owner: {collection.ownerEmail}</p>
             <p>Likes: {collection.likes}</p>
-            {collection.image === "" || collection.image === null ? (
-              ""
-            ) : (
-              <img width={100} height={100} src={collection.image} />
+            {collection.image && (
+              <img width={100} height={100} src={collection.image} alt="Collection" />
             )}
-             <button
-              onClick={() => this.handleVisitUserProfile(collection.ownerEmail)}
-            >
+            <button onClick={() => this.handleVisitUserProfile(collection.ownerEmail)}>
               Visit User Profile
             </button>
-            <button
-              onClick={() => this.handleCollectionClick(collection._id)}
-            >
+            <button onClick={() => this.handleCollectionClick(collection._id)}>
               View Collection
             </button>
-            <button
-              onClick={() => this.handleLikesIncrement(collection._id)}
-            >
-              Like
-            </button>
+            {!likedCollections.has(collection._id) && (
+              <button onClick={() => this.handleLikesIncrement(collection._id)}>
+                Like
+              </button>
+            )}
           </div>
         ))}
       </div>
